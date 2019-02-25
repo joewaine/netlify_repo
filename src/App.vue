6b429583d1a860842fc501c2aa0d597eab4bfc05
Joe Waine <joseph@comradeagency.com>
2019-02-25 08:15:03 -0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import Nav from './components/Nav.vue';
+// import HeaderModule from './components/HeaderModule.vue';
+export default {
+  name: 'app',
+  components: {
+    Nav
+    // HeaderModule
+  }
+}
+</script>
+<style lang="scss">
+@import "./styles/main";
+</style>
+<template>
+  <div id="app">
+  <!-- <HeaderModule /> -->
+  <Nav />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
