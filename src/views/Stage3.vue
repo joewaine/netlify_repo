f1f5b8d2ff99c68d96dcacb6bd344eb3177b3218
Joe Waine <joseph@comradeagency.com>
2019-03-22 10:23:58 -0700

    run build

--- src/views/Stage3.vue
+++ src/views/Stage3.vue
@@ -401,24 +401,24 @@ export default {
 																								}
 
 
-																								if (destination.index == 3) {
-																												function animateValue(id, start, end, duration) {
-																																var range = end - start;
-																																var current = start;
-																																var increment = end > start ? 1 : -1;
-																																var stepTime = Math.abs(Math.floor(duration / range));
-																																var obj = document.getElementById(id);
-																																var timer = setInterval(function() {
-																																				current += increment;
-																																				obj.innerHTML = current + '%';
-																																				if (current == end) {
-																																								clearInterval(timer);
-																																				}
-																																}, stepTime);
-																												}
+																								// if (destination.index == 3) {
+																								// 				function animateValue(id, start, end, duration) {
+																								// 								var range = end - start;
+																								// 								var current = start;
+																								// 								var increment = end > start ? 1 : -1;
+																								// 								var stepTime = Math.abs(Math.floor(duration / range));
+																								// 								var obj = document.getElementById(id);
+																								// 								var timer = setInterval(function() {
+																								// 												current += increment;
+																								// 												obj.innerHTML = current + '%';
+																								// 												if (current == end) {
+																								// 																clearInterval(timer);
+																								// 												}
+																								// 								}, stepTime);
+																								// 				}
 
-																												animateValue("value", 50, 96, 1000);
-																								}
+																								// 				animateValue("value", 50, 96, 1000);
+																								// }
 
 																								var sectionName = '.fade-opacity-' + destination.index;
 																								var opacity = document.querySelectorAll(sectionName);
